3cd7a9a3f4450ce185f758cf9ebc592d909ba634
Mark ten Klooster <tenklooster.mark@gmail.com>
2017-05-10 14:37:54 +0200

    Mainactivity gemaakt

---
+++ app/src/main/java/pt12/frigidarium/MainActivity.java
@@ -0,0 +1,13 @@
+package pt12.frigidarium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
