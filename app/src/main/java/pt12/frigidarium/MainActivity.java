5c47d44bff2b5c54394062ad3f0a07c1b3b3ed0d
Mark ten Klooster <tenklooster.mark@gmail.com>
2017-05-11 17:01:19 +0200

    Id's van menu-items gewijzigd

--- app/src/main/java/pt12/frigidarium/MainActivity.java
+++ app/src/main/java/pt12/frigidarium/MainActivity.java
@@ -3,6 +3,7 @@ package pt12.frigidarium;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -82,9 +83,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_stocklist) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_shoppinglist) {
 
         } else if (id == R.id.nav_manage) {
 
