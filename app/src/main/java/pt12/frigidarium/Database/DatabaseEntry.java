53e7f9aa38b55b24064de3d9f8af6a63810c9d4d
njittam <git@mattijnkreuzen.nl>
2017-05-11 13:12:24 +0200

    Interface met firebase aangemaakt.

---
+++ app/src/main/java/pt12/frigidarium/Database/DatabaseEntry.java
@@ -0,0 +1,72 @@
+package pt12.frigidarium.Database;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * This class automaticly updates the value from the data base.
+ * @param <O> the type of the owner of the value.
+ * @param <V> the type of the value.
+ */
+public class DatabaseEntry<O extends DatabaseEntryOwner,V> {
+    private final String name;
+    private final DatabaseReference ref;
+    private V value;
+    private final O owner;
+    private final Set<OnChangeListener<O,V>> listeners;
+
+    protected DatabaseEntry(final String name, final O owner, DatabaseReference ref) {
+        this.name = name;
+        this.owner = owner;
+        this.ref = ref;
+        listeners = new HashSet<>();
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getKey().equals(name)){
+                    value = (V) dataSnapshot.getValue();
+                    DatabaseEntryOwner.OnFinishedListener fListener = new DatabaseEntryOwner.OnFinishedListener() {
+                        @Override
+                        public void onFinished(DatabaseEntryOwner unused) {
+                                for (OnChangeListener<O,V> listener :listeners) {
+                                    listener.OnChange(owner,name,value);
+                                }
+                        }
+                    };
+                    if(owner.isFinished(name)) {
+                        fListener.onFinished(owner);
+                    }else {
+                        owner.addOnFinishedListener(fListener);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //todo handle databaseError
+            }
+        });
+    }
+    public void setValue(V value){
+        ref.setValue(value);
+    }
+    public void addListener(OnChangeListener<O,V> listener){
+        listeners.add(listener);
+    }
+
+    public void removeListener(OnChangeListener<O,V> listener){
+        listeners.remove(listener);
+    }
+
+    public interface OnChangeListener<O extends DatabaseEntryOwner,V> {
+        public void OnChange(O owner, String name, V value);
+    }
+
+}
---
+++ app/src/main/java/pt12/frigidarium/Database/DatabaseEntryOwner.java
@@ -0,0 +1,61 @@
+package pt12.frigidarium.Database;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by mattijn on 11/05/17.
+ */
+
+public class DatabaseEntryOwner {
+    private Map<String, DatabaseEntry> entries;
+    private Map<String, Boolean> entriesDone;
+    private Set<OnFinishedListener> finishedListeners;
+    private boolean isFinished = true;
+
+    /**
+     * @param name the name of the  value that was downloaded
+     * @return true if this was the last element to be added. false if at least 1 value still has not been downloaded.
+     */
+    protected boolean isFinished(String name){
+        if (entriesDone.containsKey(name)){
+            entriesDone.put(name, true);
+            return isFinished();
+        }
+        return isFinished();
+    }
+
+    /**
+     * check if all values have been downloaded from firebase
+     * this prevents null reference errors
+     * @return true if all values have been downloaded
+     */
+    public boolean isFinished(){
+        if (isFinished){
+            return true;
+        }
+        for (Map.Entry<String, Boolean> entry : entriesDone.entrySet()){
+            if(!entry.getValue()){
+                return false;
+            }
+        }
+        isFinished = true;
+        for (OnFinishedListener listener : finishedListeners){
+            listener.onFinished(this);
+        }
+        return true;
+    }
+
+    protected boolean addOnFinishedListener(OnFinishedListener listener){
+        if (!isFinished()){
+            finishedListeners.add(listener);
+            return true;
+        }
+        return false;
+    }
+
+    protected interface OnFinishedListener{
+        public void onFinished(DatabaseEntryOwner owner);
+    }
+
+}
