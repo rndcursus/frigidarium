544cc8963f530c058e3bc7cfb185b7b241bf0587
Mark ten Klooster <tenklooster.mark@gmail.com>
2017-06-01 11:35:22 +0200

    timestamp toegevoegd

--- app/src/main/java/pt12/frigidarium/database2/models/StockEntry.java
+++ app/src/main/java/pt12/frigidarium/database2/models/StockEntry.java
@@ -21,7 +21,7 @@ public class StockEntry {
     public StockEntry(String uid, Long best_before){
         this.product_uid  = uid;
         this.best_before = best_before;
-        this.timeAdded = 0L;
+        this.timeAdded = System.currentTimeMillis()/1000;
         this.addedByUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 }
